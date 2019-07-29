1fcd8d91af4fd8f08f4093356f1b92a91d2676ff
Alberto Rocha Pinalli <alberto.pinalli@acad.pucrs.br>
2019-07-28 22:49:24 -0300

    Criado os pacotes e classes

---
+++ listavips/src/main/java/br/com/pinalli/listavip/controller/ConvidadoController.java
@@ -0,0 +1,32 @@
+package br.com.pinalli.listavip.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.pinalli.listavip.models.Convidado;
+import br.com.pinalli.listavip.repository.ConvidadoRepository;
+
+@Controller
+public class ConvidadoController {
+	
+	@Autowired
+	private ConvidadoRepository repository;
+	
+	@RequestMapping("/")
+	public String index(){
+		return "index";
+	}
+	
+	@RequestMapping("listaconvidados")
+	public String listaConvidados(Model model){
+		
+		Iterable<Convidado> convidados = repository.findAll();
+		
+		model.addAttribute("convidados", convidados);
+		
+		return "listaconvidados";
+	}
+
+}
